69d7f9132cf8ce83ae141374cb9fdfdfee10dbef
Hunter Oppel <hun.oppel@gmail.com>
2020-07-17 11:41:24 -0400

    Finished ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,15 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.storage = []
+        self.capacity = capacity
+        self.current = 0
 
     def append(self, item):
-        pass
+        if len(self.storage) == self.capacity:
+            self.storage[self.current] = item
+            self.current = (self.current+1) % self.capacity
+        else:
+            self.storage.append(item)
 
     def get(self):
-        pass
+        return self.storage
