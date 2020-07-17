fce9b905dacf1c7de3b7225b1fad6667b549bfda
Hunter Oppel <hun.oppel@gmail.com>
2020-07-17 13:46:04 -0400

    Finished stretch goal for names

--- names/names.py
+++ names/names.py
@@ -71,3 +71,8 @@ print (f"runtime: {end_time - start_time} seconds")
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  There are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+# This is the fastest way I could find to do it however it does not find duplicates within the contained files and only
+# the duplicates across the 2 files.
+for name in set(names_1).intersection(names_2):
+    duplicates.append(name)
